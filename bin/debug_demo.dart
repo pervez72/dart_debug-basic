c1524e88b4d7574cd582a1a5b9ee1bd1b4dc38fd
pervez-hasan <phparvez71@gmail.com>
2023-01-08 20:13:04 +0600

    debuging process

---
+++ bin/debug_demo.dart
@@ -0,0 +1,21 @@
+import 'package:debug_demo/debug_demo.dart' as debug_demo;
+
+void main(){
+
+
+  print("A");
+  print("b");
+  print("c");
+  print("d");
+  print("e");
+  print("f");
+  math();
+
+}
+void math(){
+
+  int a=0;
+  int b=20;
+  int sum=b+a;
+  print(sum);
+}
---
+++ test/debug_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:debug_demo/debug_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
